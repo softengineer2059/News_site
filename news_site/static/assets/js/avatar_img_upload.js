890bb15244faa18f0fdd0c7878a0faa89fbf0a7c
softengineer2059 <78347206+softengineer2059@users.noreply.github.com>
2025-10-27 13:40:42 +0300

    Delete news_site directory

--- news_site/static/assets/js/avatar_img_upload.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('file-input').addEventListener('change', function() {
-    // Автоматически отправляем форму при выборе файла
-    document.getElementById('avatar-form').submit();
-});
